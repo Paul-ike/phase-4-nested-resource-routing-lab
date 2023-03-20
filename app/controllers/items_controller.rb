7bb1e5c3a37f58e7693a14e9777451e4c54e02e8
Paul <paul.ngugi@student.moringaschool.com>
2023-03-20 11:30:35 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
-rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
     if params[:user_id]
-      user = User.find_by(params[:user_id])
+      user = User.find(params[:user_id])
       items = user.items
     else
       items = Item.all
@@ -11,29 +11,23 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     render json: items, include: :user
   end
 
-  def create
-    if params[:user_id]
-      user = User.find_by(params[:user_id])
-      item = Item.create(item_params)
-      user.items << item
-    end
-    render json: item, status: :created
-  end
-
   def show
-    if params[:user_id]
-      item = Item.find_by(params[:id])
-    end
+    item = Item.find(params[:id])
     render json: item, include: :user
   end
 
-  private
+  def create
+    user = User.find(params[:user_id])
+    new_item = user.items.create(item_params)
+    render json: new_item, status: :created
+  end
+
   def item_params
     params.permit(:name, :description, :price)
   end
 
   def render_not_found_response
-    render json: { error: "Item not found" }, status: :not_found
+    render json: { error: "User not found" }, status: :not_found
   end
 
 end
